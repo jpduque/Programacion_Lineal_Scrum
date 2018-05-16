c6ea41f98f5e7d95f8eb2f7251a2b8aeb928c56a
Juan Duque <jduque@mcclatchyservices.com>
2018-05-15 19:25:29 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,142 @@
+from flask import Flask, request, render_template, redirect, url_for
+import optimization
+
+app = Flask(__name__)
+
+seniorDevCost = ''
+techDevCost = ''
+juniorDevCost = ''
+internDevCost = ''
+seniorQACost = ''
+techQACost = ''
+juniorQACost = ''
+internQACost = ''
+seniorDevDeliver = ''
+techDevDeliver = ''
+juniorDevDeliver = ''
+internDevDeliver = ''
+seniorQADeliver = ''
+techQADeliver = ''
+juniorQADeliver = ''
+internQADeliver = ''
+sprintPoints = ''
+projectPoints = ''
+projectBudget = ''
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+# Routing dev team costs
+@app.route('/DevTeamCost')
+def dev_team_build_up():
+    return render_template('TeamCostDev.html')
+
+
+@app.route('/DevTeamCost', methods=['POST'])
+def dev_team_build_up_post():
+    global seniorDevCost
+    global techDevCost
+    global juniorDevCost
+    global internDevCost
+    seniorDevCost = request.form['Dsenior']
+    techDevCost = request.form['Dstandard']
+    juniorDevCost = request.form['Djunior']
+    internDevCost = request.form['Dintern']
+    return redirect(url_for('qa_team_build_up'))
+
+
+# Routing qa team costs
+@app.route('/QATeamCost')
+def qa_team_build_up():
+    return render_template('TeamCostQA.html')
+
+
+@app.route('/QATeamCost', methods=['POST'])
+def qa_team_build_up_post():
+    global seniorQACost
+    global techQACost
+    global juniorQACost
+    global internQACost
+    seniorQACost = request.form['Qsenior']
+    techQACost = request.form['Qstandard']
+    juniorQACost = request.form['Qjunior']
+    internQACost = request.form['Qintern']
+    return redirect(url_for('dev_points_build_up'))
+
+
+# Routing dev team deliverable
+@app.route('/SprintPointsDev')
+def dev_points_build_up():
+    return render_template('SprintPointsDev.html')
+
+
+@app.route('/SprintPointsDev', methods=['POST'])
+def dev_points_build_up_post():
+    global seniorDevDeliver
+    global techDevDeliver
+    global juniorDevDeliver
+    global internDevDeliver
+    seniorDevDeliver = request.form['PDsenior']
+    techDevDeliver = request.form['PDstandard']
+    juniorDevDeliver = request.form['PDjunior']
+    internDevDeliver = request.form['PDintern']
+    return redirect(url_for('qa_points_build_up'))
+
+
+# Routing qa team deliverable
+@app.route('/SprintPointsQA')
+def qa_points_build_up():
+    return render_template('SprintPointsQA.html')
+
+
+@app.route('/SprintPointsQA', methods=['POST'])
+def qa_points_build_up_post():
+    global seniorQADeliver
+    global techQADeliver
+    global juniorQADeliver
+    global internQADeliver
+    seniorQADeliver = request.form['PQsenior']
+    techQADeliver = request.form['PQstandard']
+    juniorQADeliver = request.form['PQjunior']
+    internQADeliver = request.form['PQintern']
+    return redirect(url_for('project_points'))
+
+
+# Routing project deliverables
+@app.route('/ProjectPoints')
+def project_points():
+    return render_template('ProjectPoints.html')
+
+
+@app.route('/ProjectPoints', methods=['POST'])
+def project_points_post():
+    global sprintPoints
+    global projectPoints
+    global projectBudget
+    sprintPoints = request.form['PSprint']
+    projectPoints = request.form['PProject']
+    projectBudget = request.form['ProjectBudget']
+    print(sprintPoints, projectPoints)
+    return redirect(url_for('project_optimal'))
+
+
+# Routing Optimization
+@app.route('/OptimalProject')
+def project_optimal():
+    optimization.model(seniorDevCost, techDevCost, juniorDevCost, internDevCost, seniorQACost, techQACost, juniorQACost,
+                       internQACost, seniorDevDeliver, techDevDeliver, juniorDevDeliver, internDevDeliver,
+                       seniorQADeliver, techQADeliver, juniorQADeliver, internQADeliver, sprintPoints, projectPoints,
+                       projectBudget)
+
+    return render_template('Optimal.html', x1=round(optimization.x1.value[0]), x2=round(optimization.x2.value[0]),
+                           x3=round(optimization.x3.value[0]), x4=round(optimization.x4.value[0]),
+                           x5=round(optimization.x5.value[0]), x6=round(optimization.x6.value[0]),
+                           x7=round(optimization.x7.value[0]), x8=round(optimization.x8.value[0]),
+                           sprints=round(int(projectPoints)/int(sprintPoints)))
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ optimization.py
@@ -0,0 +1,45 @@
+from cvxopt.modeling import variable, op
+
+x1= variable()
+x2= variable()
+x3= variable()
+x4= variable()
+x5= variable()
+x6= variable()
+x7= variable()
+x8 = variable()
+
+def model(seniorDevCost,techDevCost, juniorDevCost, internDevCost, seniorQACost, techQACost, juniorQACost, internQACost, seniorDevDeliver, techDevDeliver, juniorDevDeliver, internDevDeliver, seniorQADeliver, techQADeliver, juniorQADeliver, internQADeliver, sprintPoints, projectPoints, projectBudget):
+    global x1
+    global x2
+    global x3
+    global x4
+    global x5
+    global x6
+    global x7
+    global x8f
+    c1 = (int(seniorDevDeliver)*x1 + int(techDevDeliver)*x2 + int(juniorDevDeliver)*x3 + int(internDevDeliver)*x4 + int(seniorQADeliver)*x5 + int(techQADeliver)*x6 + int(juniorQADeliver)*x7 + int(internQADeliver)*x8 >= int(sprintPoints))
+    c2 = ((int(seniorDevDeliver)*x1 + int(techDevDeliver)*x2 + int(juniorDevDeliver)*x3 + int(internDevDeliver)*x4 + int(seniorQADeliver)*x5 + int(techQADeliver)*x6 + int(juniorQADeliver)*x7 + int(internQADeliver)*x8)/int(sprintPoints) <= int(projectPoints))
+    c3 = (x1 + x2 + x3 + x4 >= 1)
+    c4 = (x5 + x6 + x7 + x8 >= 1)
+    c5 = (3*(x1+x2+x3+x4) <= (x5+x6+x7+x8) )
+    c6 = ((int(seniorDevCost)*x1 + int(techDevCost)*x2 + int(juniorDevCost)*x3 + int(internDevCost)*x4 + int(seniorQACost)*x5 + int(techQACost)*x6 + int(juniorQACost)*x7 + int(internQACost)*x8)*(int(projectPoints)/int(sprintPoints)) == int(projectBudget))
+    c7 = (x1 >= 0)
+    c8 = (x2 >= 0)
+    c9 = (x3 >= 0)
+    c10 = (x4 >= 0)
+    c11 = (x5 >= 0)
+    c12 = (x6 >= 0)
+    c13 = (x7 >= 0)
+    c14 = (x8 >= 0)
+    lp1 = op(int(seniorDevCost)*x1 + int(techDevCost)*x2 + int(juniorDevCost)*x3 + int(internDevCost)*x4 + int(seniorQACost)*x5 + int(techQACost)*x6 + int(juniorQACost)*x7 + int(internQACost)*x8, [c1,c2,c3,c4,c5,c6,c7,c8,c9,c10,c11,c12,c13,c14])
+    lp1.solve('glpk')
+    print('\nEstado: {}'.format(lp1.status))
+    print('Óptimo x1: {}'.format(round(x1.value[0])))
+    print('Óptimo x2: {}'.format(round(x2.value[0])))
+    print('Óptimo x3: {}'.format(round(x3.value[0])))
+    print('Óptimo x4: {}'.format(round(x4.value[0])))
+    print('Óptimo x5: {}'.format(round(x5.value[0])))
+    print('Óptimo x6: {}'.format(round(x6.value[0])))
+    print('Óptimo x7: {}'.format(round(x7.value[0])))
+    print('Óptimo x8: {}'.format(round(x8.value[0])))
